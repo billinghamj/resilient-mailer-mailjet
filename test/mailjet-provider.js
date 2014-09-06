33a513e9a8d55f43adb277658779c00d6d36083b
James Billingham <james@billingham.net>
2014-09-06 18:50:04 +0100

    Added initial API tests

--- test/mailjet-provider.js
+++ test/mailjet-provider.js
@@ -42,3 +42,108 @@ test('invalid message returns error', function (t) {
 	provider.mail({}, function (error) { t.notEqual(typeof error, 'undefined'); });
 	provider.mail({to:['']}, function (error) { t.notEqual(typeof error, 'undefined'); });
 });
+
+test('handles api errors correctly', function (t) {
+	var message = {
+		from: 'no-reply@example.com',
+		to: ['user@example.net', 'user@example.org'],
+		cc: ['user2@example.net'],
+		bcc: ['user3@example.net'],
+		replyto: 'info@example.com',
+		subject: 'testing, 123...',
+		textBody: 'please disregard',
+		htmlBody: '<p>please disregard</p>'
+	};
+
+	t.plan(4);
+
+	var server = setupTestServer(t,
+		function (request, response, body) {
+			var error = JSON.stringify({ message: 'generic fail' });
+
+			response.writeHead(503, { 'Content-Length': error.length });
+			response.write(error);
+			response.end();
+		},
+
+		function (addr) {
+			var options = {
+				apiSecure: false,
+				apiHostname: addr.address,
+				apiPort: addr.port
+			};
+
+			var provider = new MailjetProvider('api-key', 'api-secret', options);
+
+			provider.mail(message, function (error) {
+				t.notEqual(typeof error, 'undefined');
+				t.equal(error.httpStatusCode, 503);
+
+				server.close();
+			});
+		});
+});
+
+test('check lack of callback', function (t) {
+	var message = {
+		from: 'no-reply@example.com',
+		to: ['user@example.net', 'user@example.org'],
+		cc: ['user2@example.net'],
+		bcc: ['user3@example.net'],
+		replyto: 'info@example.com',
+		subject: 'testing, 123...',
+		textBody: 'please disregard',
+		htmlBody: '<p>please disregard</p>'
+	};
+
+	t.plan(2);
+
+	var server = setupTestServer(t,
+		function (request, response, body) {
+			var error = JSON.stringify({ message: 'generic fail' });
+
+			response.writeHead(503, { 'Content-Length': error.length });
+			response.write(error);
+			response.end();
+
+			server.close();
+		},
+
+		function (addr) {
+			var options = {
+				apiSecure: false,
+				apiHostname: addr.address,
+				apiPort: addr.port
+			};
+
+			var provider = new MailjetProvider('api-key', 'api-secret', options);
+
+			provider.mail(message);
+		});
+});
+
+// will generate 2 assertions
+function setupTestServer(t, handler, callback) {
+	var server = http.createServer(function (request, response) {
+		t.equal(request.method, 'POST');
+		t.equal(request.url, '/v3/send/message');
+
+		body = '';
+
+		request.on('data', function (chunk) {
+			body += chunk;
+		});
+
+		request.on('end', function () {
+			handler(request, response, body);
+		});
+	});
+
+	server.listen(function () {
+		var addr = server.address();
+
+		callback(addr);
+	});
+
+	return server;
+}
